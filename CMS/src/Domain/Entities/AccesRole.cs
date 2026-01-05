f76d5076fa005a365c83fa06cb5e44bcc44d94df
Back En <devs.extreme@gmail.com>
2026-01-05 16:04:46 -0500

    Ultimos cambios del cms

--- CMS/src/Domain/Entities/AccesRole.cs
+++ CMS/src/Domain/Entities/AccesRole.cs
@@ -1,12 +1,20 @@
-﻿namespace CMS.src.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CMS.src.Domain.Entities
 {
     public class AccessRole
     {
+        [Column("id")]
         public int Id { get; set; }
+        [Column("name_rol")]
         public string NameRol { get; set; } = string.Empty;
+        [Column("can_view")]
         public bool CanView { get; set; }
+        [Column("can_create")]
         public bool CanCreate { get; set; }
+        [Column("can_edit")]
         public bool CanEdit { get; set; }
+        [Column("can_delete")]
         public bool CanDelete { get; set; }
 
         //Collection
